34deae2886688ba501c26c87d989fc6511738316
nobe4 <haffreinguev@gmail.com>
2020-05-26 21:53:28 +0200

    Finally fix the tcp label background
    It was an image all along!

--- watch.py
+++ watch.py
@@ -7,6 +7,7 @@ from os.path import join
 destination = ".."
 source = "src"
 rtconfig = "rtconfig.txt"
+theme = "Mnml.ReaperTheme"
 
 dir_img = join(destination, "Mnml")
 
@@ -14,6 +15,7 @@ dir_img = join(destination, "Mnml")
 while True:
     try:
         copyfile(join(source, rtconfig), join(dir_img, rtconfig))
+        copyfile(join(source, theme), join(destination, theme))
     except:
         pass
     sleep(3)
