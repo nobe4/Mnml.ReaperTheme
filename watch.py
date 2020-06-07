1bcb5d6701c88ba15517a71fc27b9f0ffcc14a7a
nobe4 <haffreinguev@gmail.com>
2020-06-07 17:50:43 +0200

    Now work only from src, everything is copied automatically

--- watch.py
+++ watch.py
@@ -6,20 +6,23 @@ from shutil import copyfile, copytree, rmtree
 from os.path import join
 from jinja2 import Template
 from json import loads, dumps
+from os import listdir
+from os.path import isfile, join
 
 destination = ".."
 source = "src"
+
 rtconfig = "rtconfig.txt"
 template = "Mnml.ReaperTheme.jinja"
 theme = "Mnml.ReaperTheme"
 data = "theme.json"
 
-dir_img = join(destination, "Mnml")
-
-template_data = {"elements": {}}
-
+dir_img_src = join(source, "images")
+dir_img_dest = join(destination, "Mnml")
 
 def render_theme():
+    template_data = {"elements": {}}
+
     # Load data from the theme
     with open(join(source, data), "r") as f:
         data_parsed = loads(f.read())
@@ -51,11 +54,18 @@ def render_theme():
     with open(join(destination, theme), "w") as f:
         f.write(t.render(template_data))
 
+def copy_images():
+    for f in listdir(dir_img_src):
+        file_path = join(dir_img_src, f)
+        if isfile(file_path):
+            copyfile(file_path, join(dir_img_dest, f))
 
 while True:
     try:
-        copyfile(join(source, rtconfig), join(dir_img, rtconfig))
+        copyfile(join(source, rtconfig), join(dir_img_dest, rtconfig))
+        copy_images()
         render_theme()
+        print(".", end="", flush=True)
     except Exception as ex:
         print(ex)
-    sleep(3)
+    sleep(1)
